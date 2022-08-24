f042e33c887cdd1a13ff90abc5879440ee1ddf2f
Umesh Gajjala <umeshreddy1996.ur@gmail.com>
2022-08-24 12:09:51 +0530

    added case component

--- SS/src/app/app.module.ts
+++ SS/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SsHomeComponent } from './ss-home/ss-home.component';
+import { CaseComponent } from './case/case.component';
 const routes = [{path:'',component:SsHomeComponent,children:[
 {path:'',loadModule:()=> import('./tickets/tickets.module').then(m=>m.TicketsModule)}]}]
 @NgModule({
   declarations: [
     AppComponent,
-    SsHomeComponent
+    SsHomeComponent,
+    CaseComponent
   ],
   imports: [
     BrowserModule,RouterModule.forRoot([...routes])
